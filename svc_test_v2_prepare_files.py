cf15e6d598458c58f32adcd38ce9f6d88aa0f906
sdiving777 <126162853+sdiving777@users.noreply.github.com>
2023-02-23 14:29:25 +0300

    Create svc_test_v2_prepare_files.py

---
+++ svc_test_v2_prepare_files.py
@@ -0,0 +1,79 @@
+from sklearn import svm
+import random
+import sys
+
+_random_source = open("/dev/random","rb")
+
+def GetBin(the_num,sign_ammount):
+    
+    result_arr = []
+    
+    
+    for i in range(0,sign_ammount):
+        if (the_num&1 == 1):
+            result_arr.append(1)
+            #print("1",end="")
+        else:    
+            result_arr.append(0)
+            #print("0",end="")
+        the_num = the_num>>1
+    
+    return result_arr
+
+
+arrMetterBits = [93,218,230,177] # Just random numbers
+hashRL = {}
+for i in range(2**4):
+	if (i>3 and i<12):
+		print("R:",GetBin(i,4))
+		sTmp = ""
+		for s in GetBin(i,4):
+			sTmp += str(s)
+		hashRL[sTmp]=0
+	else:	
+		print("L:",GetBin(i,4))
+		sTmp = ""
+		for s in GetBin(i,4):
+			sTmp += str(s)
+		hashRL[sTmp]=1
+
+sys.exit(0)
+
+fF = open("svc_v2_train_file.txt","w") 
+
+for isn in range(108*5):
+    
+    rand_bytes = _random_source.read(32)
+    rand_num = int.from_bytes(rand_bytes,"big",signed=False)
+    arrBin = GetBin(rand_num,256)
+
+    sTmp = ""
+    for i in arrMetterBits:
+        sTmp += str(arrBin[i])
+
+    fF.write(str(hashRL.get(sTmp))+";")
+    for i in arrBin:
+        fF.write(str(i)+";")
+    fF.write("\n")
+
+fF.close()
+
+fF = open("svc_v2_test_file.txt","w") 
+
+for isn in range(108*5):
+    
+    rand_bytes = _random_source.read(32)
+    rand_num = int.from_bytes(rand_bytes,"big",signed=False)
+    arrBin = GetBin(rand_num,256)
+
+    sTmp = ""
+    for i in arrMetterBits:
+        sTmp += str(arrBin[i])
+
+    fF.write(str(hashRL.get(sTmp))+";")
+    for i in arrBin:
+        fF.write(str(i)+";")
+    fF.write("\n")
+
+fF.close()
+
