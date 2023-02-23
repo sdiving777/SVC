0f5b9af558ba79ef68df22a1a5db5afb56a509ff
sdiving777 <126162853+sdiving777@users.noreply.github.com>
2023-02-23 14:33:24 +0300

    Update svc_test_v2_prepare_files.py

--- svc_test_v2_prepare_files.py
+++ svc_test_v2_prepare_files.py
@@ -25,27 +25,24 @@ arrMetterBits = [93,218,230,177] # Just random numbers
 hashRL = {}
 for i in range(2**4):
 	if (i>3 and i<12):
-		print("R:",GetBin(i,4))
 		sTmp = ""
 		for s in GetBin(i,4):
 			sTmp += str(s)
 		hashRL[sTmp]=0
 	else:	
-		print("L:",GetBin(i,4))
 		sTmp = ""
 		for s in GetBin(i,4):
 			sTmp += str(s)
 		hashRL[sTmp]=1
 
-sys.exit(0)
 
 fF = open("svc_v2_train_file.txt","w") 
 
 for isn in range(108*5):
     
-    rand_bytes = _random_source.read(32)
+    rand_bytes = _random_source.read(128)
     rand_num = int.from_bytes(rand_bytes,"big",signed=False)
-    arrBin = GetBin(rand_num,256)
+    arrBin = GetBin(rand_num,1024)
 
     sTmp = ""
     for i in arrMetterBits:
@@ -62,9 +59,9 @@ fF = open("svc_v2_test_file.txt","w")
 
 for isn in range(108*5):
     
-    rand_bytes = _random_source.read(32)
+    rand_bytes = _random_source.read(128)
     rand_num = int.from_bytes(rand_bytes,"big",signed=False)
-    arrBin = GetBin(rand_num,256)
+    arrBin = GetBin(rand_num,1024)
 
     sTmp = ""
     for i in arrMetterBits:
