1d3b6f503435a11d79381d690e8c0e1b66b20682
sdiving777 <126162853+sdiving777@users.noreply.github.com>
2023-02-24 17:04:51 +0300

    Create apriory_test.py

---
+++ apriory_test.py
@@ -0,0 +1,28 @@
+from apriori_python import apriori
+
+
+fF = open("svc_v2_train_file.txt","r") 
+
+Y_arr_train = []
+
+for sLine in fF:
+    arrTmp = sLine.split(";")
+    arrTmp.pop(-1) 
+
+    
+    arrY = []
+    for i in range(1,len(arrTmp)):
+        if (int(arrTmp[i])==1):
+            arrY.append(str(i))
+
+    Y_arr_train.append(arrY)
+    
+fF.close()
+
+
+freqItemSet, rules = apriori(Y_arr_train, minSup=0.4, minConf=0.2) 
+for i in rules:
+	print(i)
+
+	
+	
