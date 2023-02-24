5d138d3a6d7f9baf94e3e546050067e2df48536f
sdiving777 <126162853+sdiving777@users.noreply.github.com>
2023-02-24 16:02:29 +0300

    Update svc_test_v2_prepare_files.py

--- svc_test_v2_prepare_files.py
+++ svc_test_v2_prepare_files.py
@@ -2,13 +2,24 @@ from sklearn import svm
 import random
 import sys
 
+
+iRowNum = 1000
+iRandBitsNum = 20
+iColNum = iRandBitsNum*8
+
+arrMetterBits = [5,22]#,93]#,157] # All numbers must be less then iColNum
+
+for i in arrMetterBits:
+    if (i>iColNum):
+        print("Numbers in arrMetterBits must be less then iColNum.")
+        sys.exit(0)
+    
 _random_source = open("/dev/random","rb")
 
 def GetBin(the_num,sign_ammount):
     
     result_arr = []
-    
-    
+        
     for i in range(0,sign_ammount):
         if (the_num&1 == 1):
             result_arr.append(1)
@@ -21,28 +32,30 @@ def GetBin(the_num,sign_ammount):
     return result_arr
 
 
-arrMetterBits = [93,218,230,177] # Just random numbers
 hashRL = {}
-for i in range(2**4):
-	if (i>3 and i<12):
+for i in range(2**len(arrMetterBits)):
+	if (i>=(2**len(arrMetterBits))/4 and i<((2**len(arrMetterBits))/4+(2**len(arrMetterBits))/2)):
+		print("Class 0:",GetBin(i,len(arrMetterBits)))
 		sTmp = ""
-		for s in GetBin(i,4):
+		for s in GetBin(i,len(arrMetterBits)):
 			sTmp += str(s)
 		hashRL[sTmp]=0
 	else:	
+		print("Class 1:",GetBin(i,len(arrMetterBits)))
 		sTmp = ""
-		for s in GetBin(i,4):
+		for s in GetBin(i,len(arrMetterBits)):
 			sTmp += str(s)
 		hashRL[sTmp]=1
 
 
-fF = open("svc_v2_train_file.txt","w") 
+sTf = "svc_v2_train_file.txt"
+fF = open(sTf,"w") 
 
-for isn in range(108*5):
+for isn in range(iRowNum):
     
-    rand_bytes = _random_source.read(128)
+    rand_bytes = _random_source.read(iRandBitsNum)
     rand_num = int.from_bytes(rand_bytes,"big",signed=False)
-    arrBin = GetBin(rand_num,1024)
+    arrBin = GetBin(rand_num,iRandBitsNum*8)
 
     sTmp = ""
     for i in arrMetterBits:
@@ -55,13 +68,14 @@ for isn in range(108*5):
 
 fF.close()
 
-fF = open("svc_v2_test_file.txt","w") 
+sCf = "svc_v2_test_file.txt"
+fF = open(sCf,"w") 
 
-for isn in range(108*5):
+for isn in range(iRowNum):
     
-    rand_bytes = _random_source.read(128)
+    rand_bytes = _random_source.read(iRandBitsNum)
     rand_num = int.from_bytes(rand_bytes,"big",signed=False)
-    arrBin = GetBin(rand_num,1024)
+    arrBin = GetBin(rand_num,iRandBitsNum*8)
 
     sTmp = ""
     for i in arrMetterBits:
@@ -73,4 +87,10 @@ for isn in range(108*5):
     fF.write("\n")
 
 fF.close()
+
+print()
+print("Created files:")
+print("   "+sTf)
+print("   "+sCf)
+
 
